31310db35098bb965de9d78a0426b35ae1d618a3
chumeng <2595248810@qq.com>
2024-03-24 21:43:38 +0800

    add media key bindings

--- config.h
+++ config.h
@@ -72,40 +72,46 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browsercmd[]  = { "microsoft-edge-stable", NULL };
+static const char *nextcmd[] = { "spotifyctl", "next", NULL };
+static const char *prevcmd[] = { "spotifyctl", "previous", NULL };
+static const char *pausecmd[] = { "spotifyctl", "playpause", NULL };
 
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_c,      spawn,          {.v = browsercmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	/* modifier                     key               function        argument */
+	{ MODKEY,                       XK_p,             spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,        spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_c,             spawn,          {.v = browsercmd } },
+	{ MODKEY,                       XK_grave,         togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_b,             togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_j,             rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,             rotatestack,    {.i = -1 } },
+	{ MODKEY,                       XK_j,             focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,             focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,             incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,             incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,             setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,             setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return,        zoom,           {0} },
+	{ MODKEY,                       XK_Tab,           view,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,             killclient,     {0} },
+	{ MODKEY,                       XK_t,             setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,             setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,             setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_f,             fullscreen,     {0} },
+	{ MODKEY,                       XK_space,         setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,         togglefloating, {0} },
+	{ MODKEY,                       XK_0,             view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,             tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,         focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,        focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,         tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,        tagmon,         {.i = +1 } },
+    { 0,                            XF86XK_AudioPlay, spawn,          {.v = pausecmd } },
+    { 0,                            XF86XK_AudioNext, spawn,          {.v = nextcmd } }, 
+    { 0,                            XF86XK_AudioPrev, spawn,          {.v = prevcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
